da65a0a053751a9e9920be6aaf8ff1f0330dae23
achuthanadarsh977 <160232793+achuthanadarsh977@users.noreply.github.com>
2025-07-24 11:30:42 +0530

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -1,66 +1,38 @@
--- In this SQL file, write (and comment!) the schema of your database, including the CREATE TABLE, CREATE INDEX,CREATE VIEW, etc. statements that compose it
-
-
-CREATE TABLE users(
-user_id TEXT PRIMARY KEY,
-username TEXT NOT NULL UNIQUE,
-email TEXT NOT NULL UNIQUE,
-password_hash TEXT NOT NULL
+CREATE TABLE Passengers(
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    age INT NOT NULL
 );
 
-CREATE TABLE ratings(
-rating_id TEXT PRIMARY KEY,
-user_id TEXT NOT NULL,
-recipe_id TEXT NOT NULL,
-score TEXT NOT NULL,
-comment TEXT NOT NULL,
-FOREIGN KEY(user_id) REFERENCES users(user_id),
-FOREIGN KEY(recipe_id) REFERENCES recipes(recipe_id)
+INSERT INTO Passengers VALUES("Amelia","Earhart",39);
+
+CREATE TABLE "Check-Ins"(
+    DATE_TIME DATETIME,
+    FLIGHT TEXT NOT NULL
 );
 
-CREATE TABLE recipes(
-recipe_id TEXT PRIMARY KEY,
-title TEXT NOT NULL,
-description TEXT NOT NULL,
-cooking_time TEXT NOT NULL,
-serving_size TEXT NOT NULL,
-user_id TEXT NOT NULL,
-FOREIGN KEY(user_id) REFERENCES users(user_id)
+INSERT INTO "Check-Ins" VALUES("2023-08-03 15:03","Delta Flight 300");
+
+CREATE TABLE Airlines(
+    NAME TEXT NOT NULL,
+    "CONCOURSE" TEXT NOT NULL Check ("CONCOURSE" IN ('A','B','C','D','E','F','T'))
 );
 
-CREATE TABLE category(
-name TEXT NOT NULL,
-category_id TEXT PRIMARY KEY
+
+INSERT INTO AIRLINES VALUES("Delta","A");
+INSERT INTO AIRLINES VALUES("Delta","B");
+INSERT INTO AIRLINES VALUES("Delta","C");
+INSERT INTO AIRLINES VALUES("Delta","D");
+INSERT INTO AIRLINES VALUES("Delta","T");
+
+CREATE TABLE Flights(
+    FLIGHT_NUMBER INT NOT NULL,
+    AIRLINE_NAME TEXT NOT NULL,
+    DEP_CODE TEXT NOT NULL CHECK(DEP_CODE IN ('ATL','BOS')),
+    ARRIVE_CODE TEXT NOT NULL CHECK(ARRIVE_CODE IN ('ATL','BOS')),
+    DEP_DATETIME DATETIME,
+    ARRIVE_DATETIME DATETIME,
+    PRIMARY KEY(FLIGHT_NUMBER)
 );
 
-CREATE TABLE Ingredients(
-ingredient_id INT PRIMARY KEY,
-name TEXT NOT NULL,
-category TEXT NOT NULL
-);
-
-CREATE INDEX recipe_index ON recipes(recipe_id);
-CREATE INDEX user_index ON users(user_id);
-CREATE INDEX ingredient_index ON Ingredients(name);
-
-CREATE INDEX category_index ON ingredients(category);
-
-
-CREATE VIEW user_rating
-AS SELECT username , email , rating_id , recipe_id
-FROM users JOIN ratings ON users.user_id = ratings.user_id;
-
-CREATE VIEW recipe_users
-AS SELECT recipe_id , title , email , password_hash
-FROM users JOIN recipes ON users.user_id = recipes.user_id;
-
-CREATE VIEW ratings_recipes
-AS SELECT score , comment , title , description
-FROM ratings JOIN recipes ON ratings.user_id = recipes.user_id;
-
-
-
-
-
-
-
+INSERT INTO Flights VALUES(900,"Delta","ATL","BOS","2023-08-03 18:46" , "2023-08-03 21:09");
